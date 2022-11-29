7021de03ace82f129bef53218f596d1759905192
Gabi <gabi@MacPro-0169.local>
2022-11-29 17:18:04 -0300

    updating index script to create output dir

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const yaml = require('js-yaml');
 
 const result = [];
 
+const OUTPUT_DIR = './output';
+const KITS_DIR = './kits';
+
+fs.del
 fs.readdir(
-  path.join("./", 'kits'),
+  path.join(KITS_DIR),
   (err, directories) => {
     directories.forEach((d) => {
-        const metadata = yaml.load(fs.readFileSync(`./kits/${d}/metadata.yaml`, 'utf8'));
+        const metadata = yaml.load(fs.readFileSync(`${KITS_DIR}/${d}/metadata.yaml`, 'utf8'));
         try {
-          fs.copyFileSync(`./kits/${d}/readme.md`, `./output/${d}.md`);
+          fs.copyFileSync(`${KITS_DIR}/${d}/readme.md`, `${OUTPUT_DIR}/${d}.md`);
         }catch(err) {
           console.log(`error copying readme file: ${err}`);
         }
@@ -18,7 +22,11 @@ fs.readdir(
         result.push({key: d, ...metadata});
     });
 
-    fs.writeFile("./output/starter-kits.json", JSON.stringify(result), function(err) {
+    if (!fs.existsSync(OUTPUT_DIR)){
+      fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    fs.writeFile(`${OUTPUT_DIR}/starter-kits.json`, JSON.stringify(result), function(err) {
         err ? console.log(err) : 'Process complete';
     });
   }
